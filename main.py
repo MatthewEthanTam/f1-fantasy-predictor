301583d440dffcb0f6db64884d28787ee2790afc
matthewt <matthewethan.tam@gmail.com>
2025-06-10 20:21:30 +0200

    main: testing intervals

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """
 Main module for the project.    
 """
-from openf1 import OpenF1
+from src.openf1 import OpenF1
 
 if __name__ == "__main__":
     openf1 = OpenF1()
 
-    # print(openf1.get_latest_meeting())
-    print(openf1.get_latest_meetings_session_keys_with_sessions_type())
+    openf1.get_laps_per_session()
