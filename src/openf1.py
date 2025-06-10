3ac91361865ea129caa03506e889768a0a71952e
matthewt <matthewethan.tam@gmail.com>
2025-06-10 20:21:58 +0200

    src: adding laps and drivers intergration to openf1

--- src/openf1.py
+++ src/openf1.py
@@ -8,6 +8,7 @@ from urllib.parse import urlencode
 from urllib.request import urlopen
 import json
 from pandas import DataFrame
+import pandas as pd
 
 from urllib3 import HTTPResponse
 
@@ -46,13 +47,38 @@ class OpenF1:
         meetings_df.sort_values(by="date_start", inplace=True, ascending=False)
         return meetings_df.to_dict(orient="records")[0]
     
-    def get_latest_meetings_session_keys_with_sessions_type(self) -> list[dict[str, int|str]]:
+    def get_latest_meetings_session_keys_with_session_name(self) -> list[dict[str, int|str]]:
         """
         This function is used to get the latest meeting's session keys with sessions type.
         :return: A list of dictionaries with session_key and session_type.
         """
         latest_meeting: dict = self.get_latest_meeting()
         sessions_df = DataFrame(self.get_data(endpoint="sessions", query_params={"meeting_key": latest_meeting["meeting_key"]}))
-        return sessions_df[["session_key", "session_type"]].to_dict(orient="records")
+        return sessions_df[["session_key", "session_name"]].to_dict(orient="records")
+    
+    def get_laps_per_session(self):
+        """
+        This function is used to retern all laps for all sessions in a race weekend
+        : return list[dicts]
+        """
+        sessions: list[dict] = self.get_latest_meetings_session_keys_with_session_name()
+
+        for session in sessions:
+            drivers_df = DataFrame(self.get_drivers_per_session(session["session_key"]))
+            session_intervals_df: DataFrame = DataFrame(self.get_data(endpoint="laps", query_params={"session_key":session["session_key"]}))
+            session_intervals_df.sort_values("lap_duration", inplace=True)
+            if drivers_df.empty or session_intervals_df.empty:
+                print(f"drivers/ intervals are empty for: {session['session_name']}")
+                continue
+            intervals_with_drivers = pd.merge(left=session_intervals_df, right=drivers_df, on="driver_number")
+            intervals_with_drivers.to_csv(f'{session["session_name"]}.csv')
+    
+    def get_drivers_per_session(self, session_key: str) -> dict:
+        """
+        This function returns all drivers for a specific section.
+        : return
+        """
+        return self.get_data(endpoint="drivers", query_params={"session_key": session_key})
+
     
     
